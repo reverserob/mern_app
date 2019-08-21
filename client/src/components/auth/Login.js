56d6aeb1e39b334de39b262cf95c2b9d0c04e789
reverserob <robertarandazzo@outlook.com>
2019-08-21 18:00:22 +0200

    login success && login fail reducer

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react'
-import {Link} from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { login } from "../../actions/auth";
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
     const [ formData, setFormData ] = useState({
         password: '',
         email: ''
@@ -10,9 +13,13 @@ const Login = () => {
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value});
     const onSubmit = async e => {
         e.preventDefault();
-            console.log('SUCCESS : ');
-            console.log(formData)
+        login(email,password)
     };
+
+    // Redirect if logged in
+    if (isAuthenticated) {
+        return <Redirect to='/dashboard' />;
+    }
 
     return (
         <Fragment>
@@ -48,4 +55,16 @@ const Login = () => {
     )
 };
 
-export  default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+    mapStateToProps,
+    { login }
+)(Login);
